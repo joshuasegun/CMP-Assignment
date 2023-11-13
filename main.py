5bd989bdea566100f471e56315ee1d5e5fac185b
Joshuasegun <joshuasegun633@gmail.com>
2023-11-13 06:33:47 +0100

    commit

---
+++ main.py
@@ -0,0 +1,12 @@
+p = float(input("Enter p: "))
+
+r = float(input("Enter r: "))
+
+t = float(input("Enter t: "))
+
+n = float(input("Enter n: "))
+
+SI = str(p * (1 + r * t ))
+CI =p * (1 + r/n) ** n * t
+
+print("yusuf and sons " + SI)
